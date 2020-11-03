aa8d0defbcd1e2f884577b2e9768203d7d1041e6
mimidbe <51967427+mimidbe@users.noreply.github.com>
2020-11-03 13:53:35 +0100

    Update GPS_clock.py

--- GPS_clock.py
+++ GPS_clock.py
@@ -5,6 +5,6 @@ monGPS = GPS_GROVE
 
 while True:
   if monGPS.ready():
-    Clock = gps_getInformations(info=1)
+    Clock = monGPS.getInformations(info=1)
     if Clock != None:
       display.scroll(str(''.join([str(x) for x in [Clock[0], 'h', Clock[1], 'm', Clock[2], 's']])))
